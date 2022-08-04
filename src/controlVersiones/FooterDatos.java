57b20f444d02e432f71defe6c3271a6c20839524
antonio <ammj24@hotmail.com>
2022-08-04 14:27:31 +0100

    Proporciona datos al footer.

---
+++ src/controlVersiones/FooterDatos.java
@@ -0,0 +1,11 @@
+package controlVersiones;
+
+public class FooterDatos {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+		System.out.println("Footer - Link 1");
+	}
+
+}
