15d81e262355d505fa3b5d9e33421e07acd0c8b3
antonio <ammj24@hotmail.com>
2022-08-04 12:17:56 +0100

    Añadir funcionalidad para usuarios y la barra de navegación.
    Vuelca datos para usuarios y distintas opciones en el menú principal.

---
+++ src/controlVersiones/NavegacionDatos.java
@@ -0,0 +1,11 @@
+package controlVersiones;
+
+public class NavegacionDatos {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+		System.out.println("Menu 1 - Opcion 1");
+	}
+
+}
---
+++ src/controlVersiones/Usuarios.java
@@ -0,0 +1,12 @@
+package controlVersiones;
+
+public class Usuarios {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		
+		System.out.println("Usuario 1");
+
+	}
+
+}
